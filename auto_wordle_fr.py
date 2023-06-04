fd08e413b4a9cc7cbe467adea28a19650537fb71
Feymor213 <135511288+Feymor213@users.noreply.github.com>
2023-06-04 13:33:40 +0200

    Update auto_wordle_fr.py

--- auto_wordle_fr.py
+++ auto_wordle_fr.py
@@ -3,20 +3,7 @@ import PIL.ImageGrab
 import pyautogui
 import webbrowser
 import random
-import Fuck_wordle_2
 
-
-# https://www.nytimes.com/games/wordle/index.html
-# yellow - 145, 144, 45; green - 68, 125, 61; grey - 44, 44, 46 - real wordle
-# green = (121, 184, 81), yellow = (243, 194, 55), grey = (164, 174, 196), white = (255, 255, 255) - training wordle
-# SAMPLE_COORDINATES = (
-#     ((790, 280), (870, 280), (950, 280), (1030, 280), (1110, 280)),
-#     ((790, 365), (870, 365), (950, 365), (1030, 365), (1110, 365)),
-#     ((790, 450), (870, 450), (950, 450), (1030, 450), (1110, 450)),
-#     ((790, 535), (870, 535), (950, 535), (1030, 535), (1110, 535)),
-#     ((790, 620), (870, 620), (950, 620), (1030, 620), (1110, 620)),
-#     ((790, 705), (870, 705), (950, 705), (1030, 705), (1110, 705)),
-# ) - real wordle
 COLOR_VALUE_CODES = {
     (68, 125, 61): 'correct',
     (145, 144, 45): 'partially_correct',
@@ -33,6 +20,33 @@ SAMPLE_COORDINATES = (
 remaining_letters = 'qwertyuiopasdfghjklzxcvbnm'
 prohibited_letters_positions = ['', '', '', '', '']
 
+
+def compare_to_template(word, template):
+    for i in range(len(word)):
+        if template[i] == '*':
+            continue
+        if word[i] != template[i]:
+            return False
+    return True
+
+
+def compile_words(available_letters, must_contain, template, only_one_result=False):
+    with open('Database.txt', 'r') as DATABASE:
+        DATABASE = DATABASE.read().split(sep="\n")
+        result = []
+        for i in DATABASE:
+            for j in i:
+                if j not in available_letters:
+                    break
+            else:
+                if compare_to_template(i, template):
+                    occurrence_list = list(map(lambda a: a in i, must_contain))
+                    if sum(occurrence_list) == len(occurrence_list):
+                        result.append(i)
+        if only_one_result:
+            return result[0]
+        return tuple(result)
+
 def approximate_color_of(given_color, deviation=40):
     matches = []
     for i in COLOR_VALUE_CODES.keys():
@@ -42,7 +56,7 @@ def approximate_color_of(given_color, deviation=40):
         else:
             matches.append(i)
     if len(matches) != 1:
-        raise Exception('Color match fault: '+str(matches))
+        raise Exception('Color match fault: ' + str(matches))
     return matches[0]
 
 
@@ -85,7 +99,7 @@ def step(step_n):
         for i in template:
             if (i != '*') and i not in remaining_letters:
                 remaining_letters += i
-        for i in Fuck_wordle_2.compile_words(remaining_letters, must_include, template, only_one_result=False):
+        for i in compile_words(remaining_letters, must_include, template, only_one_result=False):
             for j in range(5):
                 if i[j] in prohibited_letters_positions[j]:
                     break
@@ -99,7 +113,8 @@ def step(step_n):
     pyautogui.press('enter')
     time.sleep(7)
 
+
 webbrowser.open('https://www.nytimes.com/games/wordle/index.html')
-time.sleep(7)
+time.sleep(15)
 for i in range(6):
     step(i)
